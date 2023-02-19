ab87ccb26de8a1751f96d749f74659d30f5fb573
Peggy8422 <pig2222pig222@gmail.com>
2023-02-19 22:50:15 +0800

    feat: 加上base在vite設定檔中

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/horo-weather-check/' : '/',
   plugins: [react(), svgr()],
 })
